2fdc54d392aff11502499d117305c88f05eb7a35
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:21:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# RGB LED Strip
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/rgbLedStrip?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-display-lightgrey.svg)
+
+Single-wire-based RGB LED pixels and strip.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,87 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'rgbLedStrip.name': 'RGB LED Strip',
+            'rgbLedStrip.description': 'Single-wire-based RGB LED pixels and strip.'
+        },
+        'ru': {
+            'rgbLedStrip.name': 'RGB LED Strip',
+            'rgbLedStrip.description': 'Single-wire-based RGB LED pixels and strip.'
+        },
+        'zh-cn': {
+            'rgbLedStrip.name': 'RGB LED Strip',
+            'rgbLedStrip.description': 'Single-wire-based RGB LED pixels and strip.'
+        },
+        'zh-tw': {
+            'rgbLedStrip.name': 'RGB LED Strip',
+            'rgbLedStrip.description': 'Single-wire-based RGB LED pixels and strip.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'RGBLEDSTRIP_CATEGORY': 'RGB LED Strip',
+            'RGBLEDSTRIP_INIT': 'init rgb led strip length %1 pin %2',
+            'RGBLEDSTRIP_SETPIXELCOLOR': 'rgb led set pixel %1 color %2',
+            'RGBLEDSTRIP_FILL': 'rgb led fill from pixel %1 count %2 with color %3',
+            'RGBLEDSTRIP_COLOR': 'rgb led strip color R %1 G %2 B %3',
+            'RGBLEDSTRIP_SETBRIGHTNESS': 'set rgb led brightness %1',
+            'RGBLEDSTRIP_CLEAR': 'clear all rgb led',
+            'RGBLEDSTRIP_SHOW': 'refresh rgb led display'
+        },
+        'ru': {
+            'RGBLEDSTRIP_CATEGORY': 'RGB LED Strip',
+            'RGBLEDSTRIP_INIT': 'init rgb led strip length %1 pin %2',
+            'RGBLEDSTRIP_SETPIXELCOLOR': 'rgb led set pixel %1 color %2',
+            'RGBLEDSTRIP_FILL': 'rgb led fill from pixel %1 count %2 with color %3',
+            'RGBLEDSTRIP_COLOR': 'rgb led strip color R %1 G %2 B %3',
+            'RGBLEDSTRIP_SETBRIGHTNESS': 'set rgb led brightness %1',
+            'RGBLEDSTRIP_CLEAR': 'clear all rgb led',
+            'RGBLEDSTRIP_SHOW': 'refresh rgb led display'
+        },
+        'zh-cn': {
+            'RGBLEDSTRIP_CATEGORY': 'RGB LED Strip',
+            'RGBLEDSTRIP_INIT': 'init rgb led strip length %1 pin %2',
+            'RGBLEDSTRIP_SETPIXELCOLOR': 'rgb led set pixel %1 color %2',
+            'RGBLEDSTRIP_FILL': 'rgb led fill from pixel %1 count %2 with color %3',
+            'RGBLEDSTRIP_COLOR': 'rgb led strip color R %1 G %2 B %3',
+            'RGBLEDSTRIP_SETBRIGHTNESS': 'set rgb led brightness %1',
+            'RGBLEDSTRIP_CLEAR': 'clear all rgb led',
+            'RGBLEDSTRIP_SHOW': 'refresh rgb led display'
+        },
+        'zh-tw': {
+            'RGBLEDSTRIP_CATEGORY': 'RGB LED Strip',
+            'RGBLEDSTRIP_INIT': 'init rgb led strip length %1 pin %2',
+            'RGBLEDSTRIP_SETPIXELCOLOR': 'rgb led set pixel %1 color %2',
+            'RGBLEDSTRIP_FILL': 'rgb led fill from pixel %1 count %2 with color %3',
+            'RGBLEDSTRIP_COLOR': 'rgb led strip color R %1 G %2 B %3',
+            'RGBLEDSTRIP_SETBRIGHTNESS': 'set rgb led brightness %1',
+            'RGBLEDSTRIP_CLEAR': 'clear all rgb led',
+            'RGBLEDSTRIP_SHOW': 'refresh rgb led display'
+        }
+    }
+};
